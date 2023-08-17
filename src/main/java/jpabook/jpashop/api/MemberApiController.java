cc843d29f081a2fa814aafbd6505e1929e62df8c
khm <dr94406@naver.com>
2023-08-17 21:18:42 +0900

    [feat]회원 수정 API 추가

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -47,6 +47,28 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request){
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        public String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
     @Data
     static class CreateMemberRequest {
         private String name;
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -42,13 +42,10 @@ public class MemberService {
         return memberRepository.findOne(memberId);
     }
 
-    /**
-     * 회원 수정
-     */
     @Transactional
     public void update(Long id, String name) {
         Member member = memberRepository.findOne(id);
         member.setName(name);
-    }
 
+    }
 }
